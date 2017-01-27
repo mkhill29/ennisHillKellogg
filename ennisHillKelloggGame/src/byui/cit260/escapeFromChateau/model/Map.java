9f60d85535685f4d7fd181dfed1ee591d7fb2897
Kristine Hill <Kristine Hill@KristineHill-PC>
2017-01-26 19:49:01 -0800

    New map class Jan 26, 2017

---
+++ ennisHillKelloggGame/src/byui/cit260/escapeFromChateau/model/Map.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.escapeFromChateau.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Kristine Hill
+ */
+public class Map implements Serializable{
+    
+    //class instance variables
+    private String roomName;
+    private double rowCount;
+    private double columnCount;
+
+    public Map() {
+    }
+    
+    
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public double getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(double rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public double getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(double columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.roomName);
+        hash = 11 * hash + (int) (Double.doubleToLongBits(this.rowCount) ^ (Double.doubleToLongBits(this.rowCount) >>> 32));
+        hash = 11 * hash + (int) (Double.doubleToLongBits(this.columnCount) ^ (Double.doubleToLongBits(this.columnCount) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "roomName=" + roomName + ", rowCount=" + rowCount + ", columnCount=" + columnCount + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (Double.doubleToLongBits(this.rowCount) != Double.doubleToLongBits(other.rowCount)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.columnCount) != Double.doubleToLongBits(other.columnCount)) {
+            return false;
+        }
+        if (!Objects.equals(this.roomName, other.roomName)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
