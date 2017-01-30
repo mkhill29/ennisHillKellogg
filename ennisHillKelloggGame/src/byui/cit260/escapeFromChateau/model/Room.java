5c520ae7e1ad83db151cb9fd6942314b4b000011
Heidiennis <Heidiennis1@msn.com>
2017-01-29 16:16:38 -0800

    1/28/2017 7:49pm Refreshing push

--- ennisHillKelloggGame/src/byui/cit260/escapeFromChateau/model/Room.java
+++ ennisHillKelloggGame/src/byui/cit260/escapeFromChateau/model/Room.java
@@ -14,6 +14,7 @@ import java.util.Objects;
  */
 public class Room implements Serializable {
 
+    /*private attributes;*/
     private String discription;
     private String itemsAvailable;
     private String portals;
--- ennisHillKelloggGame/src/ennishillkellogggame/EnnisHillKelloggGame.java
+++ ennisHillKelloggGame/src/ennishillkellogggame/EnnisHillKelloggGame.java
@@ -13,6 +13,7 @@ import byui.cit260.escapeFromChateau.model.Player;
 import byui.cit260.escapeFromChateau.model.Character;
 import byui.cit260.escapeFromChateau.model.Item;
 import byui.cit260.escapeFromChateau.model.Math2;
+import byui.cit260.escapeFromChateau.model.Room;
 
 /**
  *
@@ -24,11 +25,10 @@ public class EnnisHillKelloggGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         BarrelInKitchen barrelinkitchenOne = new BarrelInKitchen();
-        
+
         Player playerOne = new Player();
-       
 
         playerOne.setName("Fred Flintstone");
         playerOne.setInventory(7.00);
@@ -43,49 +43,62 @@ public class EnnisHillKelloggGame {
 
         String gameInfo = gameOne.toString();
         System.out.println(gameInfo);
-        
+
         Map mapOne = new Map();
-        
+
         mapOne.setRoomName("Dungeon");
         mapOne.setColumnCount(4);
         mapOne.setRowCount(4);
-        
+
         String roomInfo = mapOne.toString();
         System.out.println(roomInfo);
-        
-        
+
         Math mathAntidote = new Math();
-        
+
         mathAntidote.setRoomHeight(6);
         mathAntidote.setRoomLength(9);
         mathAntidote.setRoomWidth(12);
-        
+
         String mathInfo = mathAntidote.toString();
-        System.out.println(mathInfo);       
-        
+        System.out.println(mathInfo);
+
         Character characterPrisoner = new Character();
-        
+
         characterPrisoner.setCharacterType(playerInfo);
         characterPrisoner.setCharacterLocation(roomInfo);
-        
+
         String characterInfo = characterPrisoner.toString();
         System.out.println(characterInfo);
-       
+
         Item itemOne = new Item();
-        
+
         itemOne.setUse("Lantern");
         itemOne.setMagicPower("MagicPowers");
-        
+
         String itemInfo = itemOne.toString();
         System.out.println(itemInfo);
-        
+
         Math2 mathStone = new Math2();
-        
+
         mathStone.setStoneLength(5);
         mathStone.setStoneWidth(4);
         mathStone.setStoneHeight(7);
         String mathTwo = mathStone.toString();
         System.out.println(mathTwo);
-        
+
+        Room roomOne = new Room();
+
+        roomOne.setDiscription("8X8ftroom, one bared window, sink and bed");
+        roomOne.setItemsAvailable("Toilet paper, blanket, chair");
+        roomOne.setPortals("Barred Door, Barred Window");
+        roomOne.setRow(1);
+        roomOne.setColumn(2);
+        roomOne.setIsVisited(true);
+        roomOne.setRoomType("cell");
+
+        String roomOneInfo = roomOne.toString();
+        System.out.println(roomOneInfo);
+
     }
+
 }
