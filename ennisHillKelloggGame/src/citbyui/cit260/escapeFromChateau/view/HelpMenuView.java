53508d92174568db0158d7b35e21653e798b9d1b
amikellogg <amikellogg@localhost>
2017-02-16 20:21:17 -0600

    2/16/17 - View Class

---
+++ ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/HelpMenuView.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.escapeFromChateau.view;
+
+import byui.cit260.escapeFromChateau.control.GameControl;
+import ennishillkellogggame.EnnisHillKelloggGame;
+import java.util.Scanner;
+
+/**
+ *
+ * @author amikellogg
+ */
+public class HelpMenuView {
+    private String menu;
+    private String promptMessage;
+        
+        public HelpMenuView() {
+           this.menu = "\n"
+                      +"\n------------------------------------"
+                      +"\n| Help Menu                       |"
+                      +"\n------------------------------------"
+                      +"\nC - Clues"
+                      +"\nP - How to Play"
+                      +"\nM - About Movement"
+                      +"\nO - Look Around"
+                      +"\nX - Quit"
+                      +"\n------------------------------------";
+        }
+      public void displayHelpMenuView() {
+       
+          boolean done = false; // set flag to not done
+          do{
+               // prompt for and get players name
+               String menuOption = this.getMenuOption();
+               if (menuOption.toUpperCase().equals("X")) // user wants to quit
+                   return; // exit the game
+               
+               // do requested action and display the next view
+               done = this.doAction(menuOption);
+               
+          } while (!done);
+       
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = ""; // value to be returned
+        boolean valid = false; // initialize to not valid
+        
+        while (!valid) { // loop while an invalid value is entered
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+         
+            }
+            break; // end the loop
+        }  
+        
+        return value; // return the value entered
+    }
+
+    private boolean doAction(String choice) {
+        choice = choice.toUpperCase(); // convert choice to upper case
+        
+        switch (choice) {
+            case "C": // clues
+                this.clues();
+                break;
+            case "P": // how to play
+                this.howToPlay();
+                break;
+            case "M": // about movement
+                this.aboutMovement();
+                break;
+            case "O": // look around
+                this.lookAround();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        
+        return false;
+    }
+
+    private void clues() {
+            // save current game
+            GameControl.clues(EnnisHillKelloggGame.getCurrentGame());
+            
+            //display game menu
+            GameMenuView gameMenu = new GameMenuView();
+            gameMenu.displayMenu();
+    }
+
+    private void howToPlay() {
+       System.out.println("*** beginNewGame function called ***");
+    }
+
+    private void aboutMovement() {
+        System.out.println("*** loadSavedGame function called ***");
+    }
+
+    private void lookAround() {
+        System.out.println("*** help function called ***");
+    }
+
+    }
--- ennisHillKelloggGame/src/ennishillkellogggame/EnnisHillKelloggGame.java
+++ ennisHillKelloggGame/src/ennishillkellogggame/EnnisHillKelloggGame.java
@@ -15,6 +15,7 @@ import byui.cit260.escapeFromChateau.model.Item;
 import byui.cit260.escapeFromChateau.model.Math2;
 import byui.cit260.escapeFromChateau.model.Math4;
 import byui.cit260.escapeFromChateau.model.Room;
+import citbyui.cit260.escapeFromChateau.view.StartProgramView;
 
 /**
  *
@@ -22,98 +23,34 @@ import byui.cit260.escapeFromChateau.model.Room;
  */
 public class EnnisHillKelloggGame {
 
+        private static Game currentGame = null;
+        private static Player player = null;
+        
+        public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        EnnisHillKelloggGame.currentGame = currentGame;
+    }
+
+    public static Player getPlayer() {
+        return player;
+    }
+
+    public static void setPlayer(Player player) {
+        EnnisHillKelloggGame.player = player;
+    }
+    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        Player playerOne = new Player();
-
-        playerOne.setName("Fred Flintstone");
-        playerOne.setInventory(7.00);
-
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-
-        Game gameOne = new Game();
-
-        gameOne.setTotalCaptures(1);
-        gameOne.setEscapesRemaining(9);
-
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-
-        Map mapOne = new Map();
-
-        mapOne.setRoomName("Dungeon");
-        mapOne.setColumnCount(4);
-        mapOne.setRowCount(4);
-
-        String roomInfo = mapOne.toString();
-        System.out.println(roomInfo);
-
-        Math mathAntidote = new Math();
-
-        mathAntidote.setRoomHeight(6);
-        mathAntidote.setRoomLength(9);
-        mathAntidote.setRoomWidth(12);
-
-        String mathInfo = mathAntidote.toString();
-        System.out.println(mathInfo);
-
-        Character characterPrisoner = new Character();
-
-        characterPrisoner.setCharacterType(playerInfo);
-        characterPrisoner.setCharacterLocation(roomInfo);
-
-        String characterInfo = characterPrisoner.toString();
-        System.out.println(characterInfo);
-
-        Item itemOne = new Item();
-
-        itemOne.setUse("Lantern");
-        itemOne.setMagicPower("MagicPowers");
-
-        String itemInfo = itemOne.toString();
-        System.out.println(itemInfo);
-
-        Math2 mathStone = new Math2();
-
-        mathStone.setStoneLength(5);
-        mathStone.setStoneWidth(4);
-        mathStone.setStoneHeight(7);
-        String mathTwo = mathStone.toString();
-        System.out.println(mathTwo);
-
-        Room roomOne = new Room();
-
-        roomOne.setDiscription("8X8ftroom, one barred window, sink and bed");
-        roomOne.setItemsAvailable("Toilet paper, blanket, chair");
-        roomOne.setPortals("Barred Door, Barred Window");
-        roomOne.setRow(1);
-        roomOne.setColumn(2);
-        roomOne.setIsVisited(true);
-        roomOne.setRoomType("cell");
-
-        String roomOneInfo = roomOne.toString();
-        System.out.println(roomOneInfo);
-
-        MathTwo barrelinkitchenOne = new MathTwo();
-
-      
-
-        String barrelInKitchenOneInfo = barrelinkitchenOne.toString();
-        System.out.println(barrelInKitchenOneInfo);
-
-        
-        Math4 mathTinyDoor = new Math4();
-
-        mathTinyDoor.setOuncesPerMinute(20);
-        mathTinyDoor.setFuelCapacity(24);
-        mathTinyDoor.setBurnTime(16);
-
-        String mathFour = mathTinyDoor.toString();
-        System.out.println(mathFour);
+        // create StartProgramViewOrig and display the start program view
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.displayStartProgramView();
     }
 
-}
+ }
