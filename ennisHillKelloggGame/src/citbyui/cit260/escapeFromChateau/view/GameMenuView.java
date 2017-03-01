895c7c8cb13051f4e9c7632e85ad81f217ef6862
Kristine Hill <Kristine Hill@KristineHill-PC>
2017-02-28 18:39:42 -0800

    GameMenuView create method

--- ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/GameMenuView.java
+++ ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/GameMenuView.java
@@ -85,5 +85,9 @@ public class GameMenuView {
       
     }
 
+    void displayMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
 
