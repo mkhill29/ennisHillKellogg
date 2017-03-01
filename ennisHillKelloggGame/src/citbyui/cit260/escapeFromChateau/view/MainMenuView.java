24f70a09003dd5d6032607c8ef64582fe1b878d4
Heidiennis <Heidiennis1@msn.com>
2017-02-28 20:09:34 -0800

    Work on View Inventory and Game Menu classes

--- ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/MainMenuView.java
+++ ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/MainMenuView.java
@@ -107,7 +107,7 @@ public class MainMenuView {
 
         //display game menu
         GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
+        //gameMenu.displayMenu();
     }
 
     private void loadSavedGame() {
