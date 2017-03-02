3576f9adb64a74ee228c211bb675e099d35c2273
Heidiennis <Heidiennis1@msn.com>
2017-03-01 16:23:05 -0800

    Work on View Inventory and Game Menu classes

--- ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/GameMenuView.java
+++ ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/GameMenuView.java
@@ -17,13 +17,7 @@ import java.util.Scanner;
 public class GameMenuView extends View {
 
     protected String promptMessage;
-    /*public GameMenuView() {
-        //constructor
-        this.promptMessage();
-        this.promptViewMap();
-      
 
-    }*/
     public GameMenuView() {
         super("\n"
                 + "\nYou are in the Game Room"
@@ -31,58 +25,5 @@ public class GameMenuView extends View {
                 + "\nSee the Inventory of the items"
                 + "\n available to you and"
                 + "\nthe Map of the Castle.");
-         //public void displayGameMenuView() {
-        //Math question to be inserted here like in order to find where you are, answer the following Math problem
-        boolean done = false; // set flag to not done
-        do {
-            String menuOption = this.menuOption();
-            if (menuOption.toUpperCase().equals("X")) // user wants to quit
-            {
-                return; // exit the game
-            }
-            // do requested action and display the next view
-            done = this.doAction(menuOption);
-
-        } while (!done);
     }
-
-    /*private void ViewInventory(); static {
-        
-    }
-    
-    
-    private void RoomMenu(); {
-        
-        
-    }
-      private void closeMenu();  static {
-        System.out.println("*** closeMenu function called ***");
-
-    }
-     */
-    private void promptViewMap() {
-
-    }
-
-    private String gameMenuViewOption() {
-        return null;
-
-    }
-
-    public boolean doAction(String gameMenuView) {
-        return false;
-
-    }
-
-    private void displayMainMenu() {
-        MainMenuView mainMenu = new MainMenuView();
-
-        mainMenu.display();
-
-    }
-
-    void displayGameMenuView() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
 }
