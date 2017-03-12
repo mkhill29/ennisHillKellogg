dad4e0ebc5008b9b132a15862e4d0d70b29fbf03
Heidiennis <Heidiennis1@msn.com>
2017-03-12 15:53:58 -0700

    work on Item model

--- ennisHillKelloggGame/src/byui/cit260/escapeFromChateau/model/Game.java
+++ ennisHillKelloggGame/src/byui/cit260/escapeFromChateau/model/Game.java
@@ -17,7 +17,17 @@ public class Game implements Serializable{
     // Class instance variable
     private double totalCaptures;
     private double escapesRemaining;
-
+    
+    private Item[] item;
+//the above line on 22-31 
+    I just created 3/12/17
+    
+    public Item[] getItem() {
+        return item;
+    }
+    public void setItem(Item[] item) {
+        this.item = item;
+    }
     public Game() {
     }
     
