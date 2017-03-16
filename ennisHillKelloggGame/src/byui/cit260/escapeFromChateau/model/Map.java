a67cfeec3def6492baa051b750b03d6189accd45
Heidiennis <Heidiennis1@msn.com>
2017-03-15 19:49:39 -0700

    work on Assignment with Tim and Heidi update

--- ennisHillKelloggGame/src/byui/cit260/escapeFromChateau/model/Map.java
+++ ennisHillKelloggGame/src/byui/cit260/escapeFromChateau/model/Map.java
@@ -51,14 +51,14 @@ public class Map implements Serializable {
         cell2.setRow(0);
         cell2.setColumn(0);
         cell2.setDescription("You are still in a dark prison cell.");
-        rooms[0][0] = cell2;
+        rooms[0][1] = cell2;
         
         Room cell3 = new Room();
         cell3.setName("Prison Cell3");
         cell3.setRow(0);
         cell3.setColumn(0);
         cell3.setDescription("You are in a dark prison cell, still!");
-        rooms[0][0] = cell3;
+        rooms[0][2] = cell3;
         
         
         Room cell4 = new Room();
@@ -66,14 +66,14 @@ public class Map implements Serializable {
         cell4.setRow(0);
         cell4.setColumn(0);
         cell4.setDescription("You are in a dark prison cell, for the 4th time!");
-        rooms[0][0] = cell4;
+        rooms[0][3] = cell4;
         
          Room cell5 = new Room();
         cell5.setName("Prison Cell5");
         cell5.setRow(0);
         cell5.setColumn(0);
         cell5.setDescription("You are in a dark prison cell, for the 5th time!");
-        rooms[0][0] = cell5;
+        rooms[0][4] = cell5;
         
         
          Room cell6 = new Room();
@@ -81,42 +81,42 @@ public class Map implements Serializable {
         cell6.setRow(0);
         cell6.setColumn(0);
         cell6.setDescription("You are in a dark prison cell, for the 6th time!");
-        rooms[0][0] = cell6;
+        rooms[1][0] = cell6;
         
         Room guardTower = new Room();
         guardTower.setName("Guard Tower");
         guardTower.setRow(0);
         guardTower.setColumn(0);
         guardTower.setDescription("You are in the Guard Tower now.");
-        rooms[0][0] = guardTower;
+        rooms[1][1] = guardTower;
         
         Room gateHouse = new Room();
         gateHouse.setName("Gate House");
         gateHouse.setRow(0);
         gateHouse.setColumn(0);
         gateHouse.setDescription("You are in the Gate House now.");
-        rooms[0][0] = gateHouse;
+        rooms[1][2] = gateHouse;
         
         Room mainHall = new Room();
         mainHall.setName("Main Hall");
         mainHall.setRow(0);
         mainHall.setColumn(0);
         mainHall.setDescription("You are in the Main Hall.");
-        rooms[0][0] = mainHall;
+        rooms[1][3] = mainHall;
         
         Room wardensOffice = new Room();
         wardensOffice.setName("wardensOffice");
         wardensOffice.setRow(0);
         wardensOffice.setColumn(0);
         wardensOffice.setDescription("You are in the wardensOffice.");
-        rooms[0][0] = wardensOffice;
+        rooms[1][4] = wardensOffice;
         
         Room wardensQuarters = new Room();
         wardensQuarters.setName("wardensQuarters");
         wardensQuarters.setRow(0);
         wardensQuarters.setColumn(0);
         wardensQuarters.setDescription("You are in the wardensQuarters.");
-        rooms[0][0] = wardensQuarters;
+        rooms[2][0] = wardensQuarters;
         
         
         Room nGuardTower = new Room();
@@ -124,56 +124,56 @@ public class Map implements Serializable {
         nGuardTower.setRow(0);
         nGuardTower.setColumn(0);
         nGuardTower.setDescription("You are in the nGuardTower.");
-        rooms[0][0] = nGuardTower;
+        rooms[2][1] = nGuardTower;
         
-                Room Kitchen = new Room();
+        Room Kitchen = new Room();
         Kitchen.setName("Kitchen");
         Kitchen.setRow(0);
         Kitchen.setColumn(0);
         Kitchen.setDescription("You are in the Kitchen.");
-        rooms[0][0] = Kitchen;
+        rooms[2][2] = Kitchen;
         
         Room Pantry = new Room();
         Pantry.setName("Pantry");
         Pantry.setRow(0);
         Pantry.setColumn(0);
         Pantry.setDescription("You are in the Pantry.");
-        rooms[0][0] = Pantry;
+        rooms[2][3] = Pantry;
         
         Room Larder = new Room();
         Larder.setName("Larder");
         Larder.setRow(0);
         Larder.setColumn(0);
         Larder.setDescription("You are in the Larder.");
-        rooms[0][0] = Larder;
+        rooms[2][3] = Larder;
         
         Room broomCloset = new Room();
         broomCloset.setName("broomCloset");
         broomCloset.setRow(0);
         broomCloset.setColumn(0);
         broomCloset.setDescription("You are in the broomCloset.");
-        rooms[0][0] = broomCloset;
+        rooms[2][4] = broomCloset;
         
         Room Armory = new Room();
         Armory.setName("Armory");
         Armory.setRow(0);
         Armory.setColumn(0);
         Armory.setDescription("You are in the Armory.");
-        rooms[0][0] = Armory;
+        rooms[3][0] = Armory;
         
         Room storageRoom = new Room();
         storageRoom.setName("storageRoom");
         storageRoom.setRow(0);
         storageRoom.setColumn(0);
         storageRoom.setDescription("You are in the storageRoom.");
-        rooms[0][0] = storageRoom;
+        rooms[3][1] = storageRoom;
         
         Room dungeon = new Room();
         dungeon.setName("dungeon");
         dungeon.setRow(0);
         dungeon.setColumn(0);
         dungeon.setDescription("You are in the dungeon.");
-        rooms[0][0] = dungeon;
+        rooms[3][2] = dungeon;
         
            
         Room uniformCloset = new Room();
@@ -181,7 +181,7 @@ public class Map implements Serializable {
         uniformCloset.setRow(0);
         uniformCloset.setColumn(0);
         uniformCloset.setDescription("You are in the dungeon.");
-        rooms[0][0] = uniformCloset;
+        rooms[3][3] = uniformCloset;
         
                
         Room sGuardTower = new Room();
@@ -189,7 +189,7 @@ public class Map implements Serializable {
         sGuardTower.setRow(0);
         sGuardTower.setColumn(0);
         sGuardTower.setDescription("You are in the sGuardTower.");
-        rooms[0][0] = sGuardTower;
+        rooms[3][4] = sGuardTower;
         
                
         Room GuardTowerheadquarters = new Room();
@@ -197,7 +197,7 @@ public class Map implements Serializable {
         GuardTowerheadquarters.setRow(0);
         GuardTowerheadquarters.setColumn(0);
         GuardTowerheadquarters.setDescription("You are in the GuardTowerheadquarters.");
-        rooms[0][0] = GuardTowerheadquarters;
+        rooms[4][0] = GuardTowerheadquarters;
         
                
         Room wBarracks = new Room();
@@ -205,7 +205,7 @@ public class Map implements Serializable {
         wBarracks.setRow(0);
         wBarracks.setColumn(0);
         wBarracks.setDescription("You are in the EastBarracks.");
-        rooms[0][0] = wBarracks;
+        rooms[4][1] = wBarracks;
         
                
         Room eBarracks = new Room();
@@ -213,8 +213,24 @@ public class Map implements Serializable {
         eBarracks.setRow(0);
         eBarracks.setColumn(0);
         eBarracks.setDescription("You are in the East Barracks.");
-        rooms[0][0] = eBarracks;
+        rooms[4][2] = eBarracks;
         //TODO fill up the rest of the rooms
+        
+                    
+        Room nbridge = new Room();
+        nbridge.setName("North bridge");
+        nbridge.setRow(0);
+        nbridge.setColumn(0);
+        nbridge.setDescription("You are in the North bridge.");
+        rooms[4][3] = nbridge;
+        
+                           
+        Room sbridge = new Room();
+        sbridge.setName("South bridge");
+        sbridge.setRow(0);
+        sbridge.setColumn(0);
+        sbridge.setDescription("You are in the South bridge.");
+        rooms[4][4] = sbridge;
     }
 
     public Room getRoomAt(int row, int col) {
