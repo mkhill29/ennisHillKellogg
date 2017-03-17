c8c19c52ddfa5ed8e03c30b3ed0d589cfaf0bf73
Heidiennis <Heidiennis1@msn.com>
2017-03-17 12:52:30 -0700

    work on Assignment with Kristine and Ami for the Math Lock View

---
+++ ennisHillKelloggGame/src/citbyui/cit260/escapeFromChateau/view/MathLockView.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.escapeFromChateau.view;
+
+import byui.cit260.escapeFromChateau.control.MathLockControl;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Heidi Ennis
+ */
+public class MathLockView extends View {
+
+    private int triangleLockSideA;
+    private int triangleLockSideB;
+    private int triangleLockSideC;
+
+    public MathLockView() {
+
+        Random r = new Random();
+
+        triangleLockSideA = r.nextInt(10) + 1;
+        triangleLockSideB = r.nextInt(10) + 1;
+        triangleLockSideC = r.nextInt(10) + 1;
+    }
+
+    @Override
+    public void display() {
+        this.displayMessage = "\n************************************************"
+                + "\nTo enter the Armory, you must enter a four digit code into the key pad. "
+                + "\nThe code can only be obtained by answering a math question. "
+                + "\n "
+                + "\nFor example, If a triangle has one side that is 12 feet and one side "
+                + "\nthat by is 9 feet, and one side that is 22 feet, long, then you need to  "
+                + "\ncalculate half of the perimeter."
+                + "\nEnter you answer!"
+                + "\n************************************************";//To change body of generated methods, choose Tools | Templates.
+
+        this.displayMessage += ("\nThe lock's sideA is " + triangleLockSideA);
+        this.displayMessage += ("\nThe lock's sideB is " + triangleLockSideB);
+        this.displayMessage += ("\nThe room's sideC is " + triangleLockSideC);
+        this.displayMessage += ("\nPlease enter the answer: ");
+
+        String answer = getInput();
+
+        boolean isCorrect = doAction(answer);
+
+        if (isCorrect) {
+            System.out.println("Nice job!");
+        } else {
+            System.out.println("Bad job!");
+        }
+
+    }
+
+    @Override
+    public String getInput() {
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = null; // value to be returned
+        boolean valid = false; // initialize to not valid
+
+        while (!valid) { // loop while an invalid value is entered
+            System.out.println("\n" + this.displayMessage);
+
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+
+            }
+            break; // end the loop
+        }
+
+        return value; // return the value entered
+    }
+
+    @Override
+    public boolean doAction(String playerAnswer) {
+        MathLockControl mac = new MathLockControl();
+
+        double correctAnswer = mac.halfTotalPerimeter(triangleLockSideA, triangleLockSideB, triangleLockSideC);
+        double playerAnswerNumber = Double.parseDouble(playerAnswer);
+
+        return (int) correctAnswer == (int) playerAnswerNumber;
+    }
+
+}
